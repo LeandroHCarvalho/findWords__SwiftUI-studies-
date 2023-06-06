efff06f0babdad028a8a7c0d4775309341005128
Leandro Oliveira <leandrohenriqueco@hotmail.com>
2023-06-06 11:05:48 -0400

    Initial commit

---
+++ FindWords/FindWords/FindWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindWordsApp.swift
+//  FindWords
+//
+//  Created by Leandro Carvalho on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FindWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
